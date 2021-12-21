3127a51e699491dbfb0a0def540f7211ac1659bd
jai singh <jaisingh0335@gmail.com>
2021-12-21 19:32:12 +0530

    fixed syntax error

--- ecommerce/ecommerce/settings.py
+++ ecommerce/ecommerce/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-m-wer=p5gwwv05m-12*mq234t9s$7kix&%fs=523eb^&gc_xyd
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = [*]
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
