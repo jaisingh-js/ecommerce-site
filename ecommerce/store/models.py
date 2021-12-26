a85fecdcccdc8ec35d1b0e87356abc12140d57ef
jaisingh-dev <jaisingh0335@gmail.com>
2021-12-26 11:27:38 +0530

    added daa models

--- ecommerce/store/models.py
+++ ecommerce/store/models.py
@@ -1,3 +1,23 @@
 from django.db import models
-
+from django.contrib.auth.models import User
 # Create your models here.
+
+#customer database model
+class Customer(models.Model):
+	#one user is binded with one customer
+	user = models.OneToOneField(User, on_delete=models.CASCADE, null=True, blank=True)
+	name = models.CharField(max_length=200, null=True)
+	email = models.CharField(max_length=200, null=True)
+
+	#return name for admin interface
+	def __str__(self):
+		return self.name
+
+
+class Product(models.Model):
+	name = models.CharField(max_length=200)
+	price = models.FloatField()
+	digital = models.BooleanField(default=False, null=True, blank=True)
+
+	def __str__(self):
+		return self.name
